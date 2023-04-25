d792dd4bac62f541fa5ac0dc851f725ac7f616f1
Documenter.jl <documenter@juliadocs.github.io>
2023-04-25 12:13:38 +0000

    build based on 32a3ce8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ERPExplorer","category":"page"},{"location":"#ERPExplorer","page":"Home","title":"ERPExplorer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ERPExplorer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ERPExplorer]","category":"page"}]
+}
